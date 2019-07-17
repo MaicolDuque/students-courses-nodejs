9bdf06ee573f59d8eb6a019296d794413d0554e1
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2019-07-17 10:03:47 -0500

    First commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+//Requires
+const mongoose = require('mongoose');
+const express = require('express')
+const http = require('http');
+const config = require('./config');
+// const configExpress = require('./config/express');
+// const configViewsPartials = require('./config/viewsPartials');
+// const routes = require("./routes/");
+
+// Connect to MongoDB
+mongoose.connect(config.mongo.uri, { useNewUrlParser: true }, (error, result) => {
+	if(error){
+		return console.log(error);
+	}
+
+	console.log("conectado!")
+});
+// mongoose.connection.on('error', (err) => {
+//   console.error('Error', 'MongoDB connection error', {
+//     data: err,
+//     time: new Date().toISOString(),
+//   });
+//   process.exit(-1);
+// });
+
+
+const app = express();
+const server = http.createServer(app);
+
+// configExpress(app);
+// configViewsPartials(app, express);
+// routes(app);
+app.get("/", (req, res) => {
+	res.send("Hello World")
+})
+
+server.listen(config.port, () => {
+	console.log (`servidor en el puerto ${config.port}`);
+});
---
+++ src/config/index.js
@@ -0,0 +1,25 @@
+/**
+ * @author: Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
+ */
+
+ const all = {
+  env: process.env.NODE_ENV,
+
+  url: "https://nodejs-and-mongoose.herokuapp.com/",
+
+  // Server port
+  port: process.env.PORT || 8080,
+
+  // Server IP
+  ip: process.env.IP || '0.0.0.0',
+  
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb+srv://maicol:maicol123456@cluster0-xc611.mongodb.net/asignaturas?retryWrites=true&w=majority',
+    db: 'asignaturas',
+  },
+ }
+
+ module.exports = all;
+
