fa52cddb46425f00a6c25d464232d8deed096128
Maicol Felipe Duque Urrea <maicolduque01@gmail.com>
2019-08-14 09:28:08 -0500

    Update socket client

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   $("#txtChat").click(()=>{
     let rigth = $("#chat").css("right");
     if(rigth == "0px"){
-      $("#chat").css("right", "-343px");   
+      $("#chat").css("right", "-23%");   
     }else{
       $("#chat").css("right", "0px");
     } 
